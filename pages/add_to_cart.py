89b10194c30f2d3d7325781aac2711d0408416c4
cherish-noe <myatnoe.luck@gmail.com>
2024-07-23 08:44:56 +0000

    Add cart recommendations

--- pages/add_to_cart.py
+++ pages/add_to_cart.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("../data/raw/sample_product_metadata.csv")
+cols = ['product_name', 'aisle', 'department']
+file_path = "../data/raw/sample_product_metadata.csv"
+df = pd.read_csv(file_path, usecols=cols)
 
-edited_df = st.data_editor(df)
+# Add 'add_to_cart' col
+df['add_to_cart'] = False
+
+def update_cart():
+    updated_data = st.session_state.add_to_cart
+    st.write(updated_data)
+
+edited_df = st.data_editor(
+    df, 
+    disabled=("product_name", "aisle", "department"),
+    key="add_to_cart",
+    on_change=update_cart
+)
+
+
+
+
